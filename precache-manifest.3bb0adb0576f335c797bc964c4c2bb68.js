0701ce3c3e8c914e465582fabaee3b2a31f923e4
feniks916 <53240482+feniks916@users.noreply.github.com>
2020-07-14 16:21:09 +0300

    Updates

---
+++ precache-manifest.3bb0adb0576f335c797bc964c4c2bb68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c841ee6496f33dfb30f14bc41952122f",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "f2edbdeed02b54d6c4bb",
+    "url": "/social-network/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ee4a151e44909b4648d8",
+    "url": "/social-network/static/js/2.d7128583.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/social-network/static/js/2.d7128583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2edbdeed02b54d6c4bb",
+    "url": "/social-network/static/js/main.84222dbc.chunk.js"
+  },
+  {
+    "revision": "73dc02b1c42065a1d3d3",
+    "url": "/social-network/static/js/runtime-main.a948e851.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social-network/static/media/logo.5d5d9eef.svg"
+  }
+]);
