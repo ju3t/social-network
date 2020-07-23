3d21d0a596d07516f7d7422b86390d2bfa297adc
Danila Molodtsov <danmol04@gmail.com>
2020-07-23 10:12:45 +0300

    base layer of friends

---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,17 @@
+const FriendsWrapper = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
+    background: #FFFFFF;
+    font-family: Montserrat;
+    border-radius: 15px 15px 0px 0px;
+    padding: 114px 114px 114px 91px;
+    margin-top: 100px;
+    position: relative;
+`
+//убрать маргин-топ при переводе на реальную страницу
+const Friends = (props) => {
+    return (
+        <FriendsWrapper>
+        </FriendsWrapper>
+    )
+}
+export default Friends;
