1d28c668f62992957fb2d19aa144106812df4a32
Danila Molodtsov <danmol04@gmail.com>
2020-07-23 11:07:19 +0300

    better icon

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Input } from 'antd'
+import styled from 'styled-components'
+import SingleFriend from './SingleFriend'
+import SearchIcon from './../../img/icons/search.svg';
+
 const FriendsWrapper = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
     background: #FFFFFF;
@@ -8,6 +16,7 @@ const FriendsWrapper = styled.div`
     position: relative;
 `
 //убрать маргин-топ при переводе на реальную страницу
+
 const PageMarker = styled.h2`
     margin: 0;
     left: 90px;
@@ -20,11 +29,52 @@ const PageMarker = styled.h2`
     line-height: 39px;
     background: #FFB11B;
 `
+
+const SearchBlock = styled.div`
+    display: flex;
+    font-size: 16px;
+    line-height: 20px;
+    align-items: center;
+    justify-content: space-between;
+    position: relative;
+    min-width: 100%;
+    border-top: 1px solid #B3B3B3;
+    border-bottom: 1px solid #B3B3B3;
+    &::after {
+        top: 49px;
+        right: 0;
+        position: absolute;
+        height: 30px;
+        width: 30px;
+        background-color: #515151;
+        mask-image: url(${SearchIcon});
+        mask-size: cover;
+        content: '';
+    }
+`
+
+const SearchInpit = styled(Input)`
+    color: #515151;
+    min-width: 100%;
+    padding-right: 30px;
+    margin: 53px 0px 57px 0px;
+    border: none;
+    outline:none;
+    font-size: 16px;
+    line-height: 20px;
+    position: relative;
+    
+`
+
 const Friends = (props) => {
     return (
         <FriendsWrapper>
             <PageMarker>Друзья</PageMarker>
+            <SearchBlock>
+                <SearchInpit placeholder='Начните поиск друзей...' nostyle='true' />
+            </SearchBlock>
         </FriendsWrapper>
     )
 }
+
 export default Friends;
