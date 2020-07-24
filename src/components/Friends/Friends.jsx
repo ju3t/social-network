6bf39ff06732303d3d07a87390fc1f398bc8dff0
Danila Molodtsov <danmol04@gmail.com>
2020-07-24 06:39:27 +0300

    better margin from top

--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -12,7 +12,7 @@ const FriendsWrapper = styled.div`
   font-family: 'Montserrat', sans-serif;
   border-radius: 15px 15px 0px 0px;
   padding: 114px 114px 114px 91px;
-  margin-top: 100px;
+  margin-top: 275px;
   position: relative;
 `;
 
