b0ab146ccbabacb0ca8b821ae70670adfb6fb1f8
feniks916 <53240482+feniks916@users.noreply.github.com>
2020-07-24 03:19:27 +0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Login from './loginPage'
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
