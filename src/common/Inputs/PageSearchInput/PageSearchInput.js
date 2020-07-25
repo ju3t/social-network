fb7c1307bccd23e70985d979a77b877faaa1f051
Danila Molodtsov <danmol04@gmail.com>
2020-07-25 18:32:55 +0300

    input replace

--- src/common/Inputs/PageSearchInput/PageSearchInput.js
+++ src/common/Inputs/PageSearchInput/PageSearchInput.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import SearchIcon from '../../../img/icons/search.svg';
+import SearchIcon from './../../img/icons/search.svg';
 import PropTypes from 'prop-types';
 import React from 'react';
 
