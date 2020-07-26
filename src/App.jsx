1cf1938ec70a786657441b6320badd7aa64a08c6
Pardeg <pardeg@gmail.com>
2020-07-26 23:33:01 +0300

    merge with dev 26.07

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import Login from './components/LoginPage/loginPage';
+import Groups from './components/Groups';
 
 const App = () => (
   <Switch>
     <Route path="/social-network" component={Login} />
+    <Route path="/groups" component={Groups} />
   </Switch>
 );
 
