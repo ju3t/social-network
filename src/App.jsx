04be57f63fab0d2b9e1a3510d5498d9f27ec29bf
Avisrara74 <avisrara74@gmail.com>
2020-07-26 22:31:03 +0500

    tabs linter fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 import Login from './components/LoginPage/loginPage';
 import Messages from './components/Messages';
-import { Switch, Route } from 'react-router-dom';
 
-const App = () => {
-  return (
-    <Switch>
-        <Route path='/social-network' component={Login} />
-        <Route path='/messages' component={Messages} />
-    </Switch>
-  );
-}
+const App = () => (
+  <Switch>
+    <Route path="/social-network" component={Login} />
+    <Route path="/messages" component={Messages} />
+  </Switch>
+);
 
 export default App;
