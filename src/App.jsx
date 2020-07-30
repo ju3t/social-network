e13c7cfaf9cfe77e7f3a75b0db6156418b181159
Danila Molodtsov <danmol04@gmail.com>
2020-07-30 10:23:01 +0300

    adding swiper and node-sass
    node-sass need for swiper correct animation work

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,14 @@ import { Switch, Route } from 'react-router-dom';
 import Login from './components/LoginPage/loginPage';
 import Main from './components/Main';
 import Friends from './components/Friends';
+import VideoPage from './components/VideoPage/VideoPage';
 
 const App = () => (
   <Switch>
     <Route path="/" component={Main} exact />
     <Route path="/social-network" component={Login} />
     <Route path="/friends" component={Friends} />
+    <Route path="/video" component={VideoPage} />
   </Switch>
 );
 
