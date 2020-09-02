dbab6930142bbb9f82f1b7cf7f2222a79c3dd9f2
Ruslan-I7 <57489028+Ruslan-I7@users.noreply.github.com>
2020-09-02 21:48:45 +0300

    update file

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,17 @@
 import { combineReducers } from 'redux';
+import { handleActions } from 'redux-actions'
+import * as actions from '../actions/actions'
 
 const reducer1 = () => 1;
 const reducer2 = () => 2;
 
-export default combineReducers({ reducer1, reducer2 });
+const audiosReducer = handleActions({
+  [actions.fetchGetAllAudiosSuccess](state, { payload }) {
+    console.log('state audiosReducer', state);
+    console.log('payload audiosReducer', payload);
+    return payload
+  }
+}, {})
+
+
+export default combineReducers({ reducer1, reducer2, audiosReducer });
