a2d04f469f7d255af3fa154e4f5e00ebc662dd3e
Ruslan-I7 <57489028+Ruslan-I7@users.noreply.github.com>
2020-09-12 14:12:13 +0300

    changed to tsx, wrappered own render in dom render  as in doc toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,38 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
-import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import reducers from './redux/reducers';
 import * as serviceWorker from './serviceWorker';
-import App from './App.jsx';
 import store from './redux-toolkit/store';
 
-// const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+const App = require('./App').default;
 
 const render = () => {
-  const App = require('./app/App').default
+  ReactDOM.render(
+    <BrowserRouter>
+      <Provider store={store}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </Provider>
+    </BrowserRouter>,
+    document.getElementById('root'),
+  );
+};
 
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
-
-render()
+render();
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
-  module.hot.accept('./app/App', render)
+  module.hot.accept('./App', render);
 }
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
